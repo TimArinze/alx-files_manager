c215e64d022bcc78077330741b9d2537f5df32d3
Arinze Nwutobo <tim.arinze@outlook.com>
2023-09-05 23:59:30 +0100

    task

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Error:', err);
+    });
+  }
+
+  isAlive() {
+	if (this.client.connected) {
+		return true;
+	} else {
+		return false;
+	}
+  }
+  async get(key) {
+	return new Promise((resolve, reject) => {
+		this.client.get(key, (err, value) => {
+			if (err) reject(err);
+			resolve(value);
+		})
+	})
+  }
+
+  async set(key, value, duration) {
+	return new Promise((resolve, reject) => {
+		this.client.setex(key, duration, value, (err) => {
+			if (err) reject(err);
+			resolve();
+		})
+  })}
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
