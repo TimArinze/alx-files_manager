1dc1fe7a58708516a705c9d0a63371075f11d688
Arinze Nwutobo <tim.arinze@outlook.com>
2023-09-07 15:31:50 +0100

    task

---
+++ controllers/AppController.js
@@ -0,0 +1,28 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redis = await redisClient.isAlive();
+    const db = await dbClient.isAlive();
+    const status = {
+      redis: `${redis}`,
+      db: `${db}`,
+    };
+    res.json(status);
+    res.status(200);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    const status = {
+      users,
+      files,
+    };
+    res.json(status);
+    res.status(200);
+  }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// Require the routes defined in routes/index.js
+const routes = require('./routes/index');
+
+app.use('/', routes);
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
