167384a32e312b7a279c09499ce45406692ccb31
Arinze Nwutobo <tim.arinze@outlook.com>
2023-09-08 12:59:33 +0100

    task

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ const UsersController = require('../controllers/UsersController');
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', UsersController.getConnect);
+router.get('/disconnect', UsersController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
