6e906a95c65da68b9115641574a6cb94ea7a7b6b
Arinze Nwutobo <tim.arinze@outlook.com>
2023-09-09 13:45:08 +0100

    task

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,19 +7,22 @@ class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      return res.json({ error: 'Missing email' }).status(400);
+      res.status(400);
+      res.json({ error: 'Missing email' });
+      return res;
     }
     if (!password) {
-      return res.json({ error: 'Missing password' }).status(400);
+      return res.status(400).json({ error: 'Missing password' });
     }
     const emailCheck = await dbClient.client.db(dbClient.database).collection('users').findOne({ email });
     if (emailCheck) {
-      return res.json({ error: 'Already exist' }).status(400);
+      return res.status(400).json({ error: 'Already exist' });
     }
     const hashpassword = sha(password);
     await dbClient.client.db(dbClient.database).collection('users').insertOne({ email, password: hashpassword });
     const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ email });
-    return res.json({ id: user._id.toString(), email: user.email }).status(201);
+    res.status(201);
+    return res.json({ id: user._id.toString(), email: user.email });
   }
 
   static async getMe(req, res) {
@@ -27,10 +30,11 @@ class UsersController {
     const key = `auth_${token}`;
     const userID = await redisClient.get(key);
     if (!token || !userID) {
-      return res.json({ error: 'Unauthorized' }).status(401);
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     const _id = new ObjectId(userID);
     const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id });
+    res.status(200);
     return res.json({ email: user.email, id: user._id.toString() });
   }
 }
